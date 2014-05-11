d2df8deaf81e5a31618558a80c0b927835e533bc
Thomas Levine <_@thomaslevine.com>
2014-05-11 19:49:28 +0000

    -

--- node-tests/test-brands.js
+++ node-tests/test-brands.js
@@ -4,6 +4,6 @@ var brands = require('../data/brands.js')
 test('replace', function (t) {
   t.plan(1)
   var observed = brands.replace('hsaoehoeu aoe  oeht Lightlife tnoaetnsoeu')
-  var expected = 'hsaoehoeu aoe  oeht Lightlife (ConAgra) tnoaetnsoeu')
-  t.equal(chunk, '0123456789')
+  var expected = 'hsaoehoeu aoe  oeht Lightlife (ConAgra) tnoaetnsoeu'
+  t.equal(observed, expected)
 })
